a9e0285ff2c5bec6eca4c48cace37d3dcabfb758
JFranz <djb4p97@gmail.com>
2026-09-06 10:52:21 +0200

    ci: update Node.js version to 22, remove basic SSL plugin
    - Upgrade GitHub Actions workflow to use Node.js 22 for deployment.
    - Remove the @vitejs/plugin-basic-ssl plugin from Vite configuration.

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/onyxV2/",
   plugins: [
-    basicSsl(),
     react(),
     VitePWA({
       registerType: 'autoUpdate',
